0192e315644afa22094ac5c63ed7bb6e27551c6a
Meacha1 <meachattd@gmail.com>
2023-06-01 01:06:05 +0300

    db_config

--- const_estimator_db.js
+++ const_estimator_db.js
@@ -1,27 +1,31 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost', // MySQL server host
-  port: 3306, // MySQL server port
-  user: 'root', // MySQL username
-  password: 'password', // MySQL password
-});
-
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL server:', err);
-    return;
-  }
-  console.log('Connected to MySQL server');
-
-  // Create the database
-  connection.query('CREATE DATABASE IF NOT EXISTS const_estimator_db', (err) => {
-    if (err) {
-      console.error('Error creating MySQL database:', err);
-      return;
-    }
-    console.log('MySQL database created');
+const setupDatabase = () => {
+  const connection = mysql.createConnection({
+    host: 'localhost', // MySQL server host
+    port: 3306, // MySQL server port
+    user: 'root', // MySQL username
+    password: 'password', // MySQL password
   });
 
-  connection.end();
-});
+  connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to MySQL server:', err);
+      return;
+    }
+    console.log('Connected to MySQL server');
+
+    // Create the database
+    connection.query('CREATE DATABASE IF NOT EXISTS const_estimator_db', (err) => {
+      if (err) {
+        console.error('Error creating MySQL database:', err);
+        return;
+      }
+      console.log('MySQL database created');
+    });
+
+    connection.end();
+  });
+};
+
+module.exports = setupDatabase;
