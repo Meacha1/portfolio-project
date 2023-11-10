b87806491728656ea473e351448e2810899ce980
Meacha1 <meachattd@gmail.com>
2023-11-10 12:15:24 +0300

    test5

--- config/db-config.js
+++ config/db-config.js
@@ -1,19 +1,19 @@
-module.exports = {
-    HOST: 'localhost',
-    USER: 'postgres',
-    PORT: 5432,
-    PASSWORD: 'mm123321123',
-    DATABASE: 'cost_estimator_db',
-    DIALECT: 'postgres',
-    APP_PORT: 3000
-}
-
 // module.exports = {
-//     HOST: 'db.lfqyzguebdqrpihxsjit.supabase.co',
+//     HOST: 'localhost',
 //     USER: 'postgres',
 //     PORT: 5432,
-//     PASSWORD: 'pSS5iy9tdEi%Xn#',
-//     DATABASE: 'postgres',
+//     PASSWORD: 'mm123321123',
+//     DATABASE: 'cost_estimator_db',
 //     DIALECT: 'postgres',
 //     APP_PORT: 3000
 // }
+
+module.exports = {
+    HOST: 'db.lfqyzguebdqrpihxsjit.supabase.co',
+    USER: 'postgres',
+    PORT: 5432,
+    PASSWORD: 'pSS5iy9tdEi%Xn#',
+    DATABASE: 'postgres',
+    DIALECT: 'postgres',
+    APP_PORT: 3000
+}
